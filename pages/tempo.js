63c755519bcd0ca7feb20f015a54d87c4671e086
Bruno Soares <bruno.soaest95@gmail.com>
2022-10-27 17:42:02 -0300

    testes

--- pages/tempo.js
+++ pages/tempo.js
@@ -1,17 +1,30 @@
 import Link from 'next/link';
 
-function Tempo(){
+function Tempo(props) {
     const dynamicDate = new Date();
     const dynamicDateString = dynamicDate.toGMTString();
 
 
     return (<div>
-       {dynamicDateString} (dinamico)
-      
-       <br></br><br></br>
-        <Link href="/">Home</Link><br></br><br></br>
+      <div> {dynamicDateString} (dinamico)</div> 
+      <div> {props.staticDateString} (static)</div> 
+
+
+        <Link href="/">Home</Link>
 
     </div>)
 }
+
+export function getStaticProps() {
+    const staticDate = new Date();
+    const staticDateString = staticDate.toGMTString();
+
+    return {
+        props: {
+            staticDateString,
+        }
+    }
+}
+
 
 export default Tempo
