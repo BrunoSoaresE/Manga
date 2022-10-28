07b34b3cd221c9e7e88eddaa711805417bb9519c
Bruno Soares <bruno.soaest95@gmail.com>
2022-10-28 18:19:04 -0300

    AddMeta

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,4 @@
+import Head from 'next/head';
 import React from 'react';
 import { SubPageCabecalho } from '../subPages/Cabecalho/Cabecalho';
 import { SubPageClientes } from '../subPages/Clientes/Clientes';
@@ -9,9 +10,25 @@ import { SubPagePerguntas } from '../subPages/Perguntas/Perguntas';
 import { SubPagePortifolio } from '../subPages/Portifolio/Portifolio';
 import { SubPageRodape } from '../subPages/Rodape/Rodape';
 
-function Home(){
-    return (<div>
- <h1>Meu teste seo</h1>
+function Home() {
+    return (<div className="application">
+
+        <Head>
+            <title>TecManga</title>
+            <meta name='description' content='Seu site está próximo de se tornar realidade' />
+            <meta property="og:title" content="Veja como ter um site sem preocupações" />
+            <meta property="og:description" content="Tenha sua marca em destaque, conseguindo atrair os mais diversos publicos." />
+
+
+            <meta property="og:image" content="https://ahrefs.com/blog/wp-content/uploads/2019/12/fb-how-to-become-an-seo-expert.png" />
+            <meta property='og:url' content='https://tecmanga.com.br/' />
+            <meta property='og:type' content='website' />
+
+            <link rel='icon' href='/favicon.ico' />
+        </Head>
+
+
+        <h1>Meu teste seo</h1>
         <SubPageCabecalho name="1 - SubPageCabecalho"></SubPageCabecalho>
         <SubPageEspaco name="2 - SubPageEspaco"></SubPageEspaco>
         <SubPageComoFunciona name="3 - SubPageComoFunciona"></SubPageComoFunciona>
@@ -20,8 +37,8 @@ function Home(){
         <SubPageClientes name="6 - SubPageClientes"></SubPageClientes>
         <SubPageFaleConosco name="7 - SubPageFaleConosco"></SubPageFaleConosco>
         <SubPagePerguntas name="8 - SubPagePerguntas"></SubPagePerguntas>
-        <SubPageRodape name="9 - SubPageRodape"></SubPageRodape>   
-      
+        <SubPageRodape name="9 - SubPageRodape"></SubPageRodape>
+
     </div>)
 }
 export default Home
