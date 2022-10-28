ed70f0ebd117ec8e4f7b73d3e97e6bc4bf39346f
Bruno Soares <bruno.soaest95@gmail.com>
2022-10-28 13:39:46 -0300

    Variavel de ambiente

--- pages/api/tempo.js
+++ pages/api/tempo.js
@@ -1,8 +1,16 @@
-function tempo(request,response){
+async function tempo(request,response){
+    const apiSecretEx = process.env.POKEMON_SECRET_API;
+
     const dynamicDate = new Date();
+
+
+    const subscribersResponse = await fetch(`https://pokeapi.co/api/v2/pokemon/${apiSecretEx}`);
+    const subscribersResponseJson = await subscribersResponse.json();
+    const pokemon = subscribersResponseJson.name;
 
     response.json({
         date: dynamicDate.toGMTString(),
+        pokemon: pokemon,
     });
 }
 
