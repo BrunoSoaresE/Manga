d090d85e7e3aa1dff5b0d211ef0b6c968558c6a8
Bruno Soares <bruno.soaest95@gmail.com>
2022-10-29 18:43:21 -0300

    Menu

--- subPages/AppBar/AppBar.js
+++ subPages/AppBar/AppBar.js
@@ -1,25 +1,43 @@
-import { CgMenu } from "react-icons/cg";
+import { useEffect, useRef } from 'react';
 
+import { CgMenu } from "react-icons/cg";
 import styles from './AppBar.module.css';
 
 export function SubPageAppBar(props) {
+
+  const refMenuToggle = useRef();
+  const refHeader = useRef();
+  useEffect(() => {
+
+  
+
+    refMenuToggle.current.onclick = function () {
+      refHeader.current.classList.toggle(styles.open);
+      console.log("DEU CERTO")
+    }
+
+  },[])
+  
+
+
   return (
-    <header className={styles.header}>
-    
-        <a href='#' className={styles.logo}>Logo</a>
-        <div className={styles.group}>
-          <ul className={styles.navigation}>
+    <header className={`${styles.header} `}   ref={refHeader} >
 
-            <li><a href="#">Home</a></li>
-            <li><a href="#">About</a></li>
-            <li><a href="#">Services</a></li>
-            <li><a href="#">Blog</a></li>
-            <li><a href="#">Contatos</a></li>
-          </ul>
-          <CgMenu className={styles.menuToggle} />
+      <a href='#' className={styles.logo}>Logo</a>
+      <div className={styles.group}>
+        <ul className={styles.navigation}>
+
+          <li><a href="#">Home</a></li>
+          <li><a href="#">About</a></li>
+          <li><a href="#">Services</a></li>
+          <li><a href="#">Blog</a></li>
+          <li><a href="#">Contatos</a></li>
+        </ul>
+        <div ref={refMenuToggle} className={styles.menuToggle}>
+        <CgMenu/>
+        </div>
 
 
-   
       </div>
 
     </header>
