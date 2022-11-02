27a786b91b4ec9d55a718ed6f6dbb1ef9ce3ce2f
Bruno Soares <bruno.soaest95@gmail.com>
2022-11-01 23:15:25 -0300

    scroll-behavior: smooth;

--- subPages/Clientes/Clientes.js
+++ subPages/Clientes/Clientes.js
@@ -1,14 +1,41 @@
-import styles from './Clientes.module.css'
-
+import Image from 'next/image';
+import trabalho from '../../assets/images/trabalho.png';
+import styles from './Clientes.module.css';
 export function SubPageClientes(props) {
   return (
-    <button
-      type="button"
-      // Note how the "error" class is accessed as a property on the imported
-      // `styles` object.
-      className={styles.error}
-    >
-     SubPageClientes - {props.name}
-    </button>
+    <div className={styles.clienteContainer}>
+      <div className={styles.clienteList}>
+        <div className={styles.clienteListScroll}>
+          <Image src={trabalho} className={styles.clienteListLogo} alt="Logo TecManga" />
+          <Image src={trabalho} className={styles.clienteListLogo} alt="Logo TecManga" />
+          <Image src={trabalho} className={styles.clienteListLogo} alt="Logo TecManga" />
+          <Image src={trabalho} className={styles.clienteListLogo} alt="Logo TecManga" />
+          <Image src={trabalho} className={styles.clienteListLogo} alt="Logo TecManga" />
+          <Image src={trabalho} className={styles.clienteListLogo} alt="Logo TecManga" />
+          <Image src={trabalho} className={styles.clienteListLogo} alt="Logo TecManga" />
+          <Image src={trabalho} className={styles.clienteListLogo} alt="Logo TecManga" />
+          <Image src={trabalho} className={styles.clienteListLogo} alt="Logo TecManga" />
+          <Image src={trabalho} className={styles.clienteListLogo} alt="Logo TecManga" />
+          <Image src={trabalho} className={styles.clienteListLogo} alt="Logo TecManga" />
+          <Image src={trabalho} className={styles.clienteListLogo} alt="Logo TecManga" />
+        </div>
+      </div>
+      <p className={styles.clienteTexto}></p>
+
+      <div className={styles.clienteComentarios}>
+        <div className={styles.clienteComentariosCard}>
+          {/*  <Image src={trabalho} className={styles.clienteComentariosFoto} alt="Logo TecManga" />*/}
+          <p className={styles.clienteComentariosNome}></p>
+          <p className={styles.clienteComentariosData}></p>
+          <p className={styles.clienteComentariosNota}></p>
+          <p className={styles.clienteComentariosLink}></p>
+          <p className={styles.clienteComentariosTexto}></p>
+        </div>
+
+
+      </div>
+
+
+    </div>
   )
 }
