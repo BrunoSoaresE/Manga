b3e6f175d936bb81f56c6776f88af3512ec1bd56
Bruno Soares <bruno.soaest95@gmail.com>
2022-11-02 13:54:59 -0300

    ajustes footer

--- subPages/Rodape/Rodape.js
+++ subPages/Rodape/Rodape.js
@@ -5,11 +5,12 @@ import styles from './Rodape.module.css';
 
 export function SubPageRodape(props) {
   return (
-    <div className={styles.Rodape}>
+    <footer className={styles.Rodape}>
       <Image src={logo} className={styles.logoImg} alt="Logo TecManga" />
       <div className={styles.RodapeDivTexto}>
-      <p className={styles.RodapeTexto} >A manga é uma agência de criação de sites com clientes satisfeitos espalhados pelo mundo inteiro. Atraves de uma equipe competente e organizada iremos fazer seu negocio decolar.</p>
+        <p className={styles.RodapeTexto} >A manga é uma agência de criação de sites com clientes satisfeitos espalhados pelo mundo inteiro. Atraves de uma equipe competente e organizada iremos fazer seu negocio decolar.</p>
       </div>
+      <hr  className={styles.RodapeHr}></hr>
       <div className={styles.Contato}>
         <div className={styles.ContatoElen}>
           <BsWhatsapp className={styles.ContatoElenIcon} />     (31) 9 8339-9638
@@ -22,6 +23,6 @@ export function SubPageRodape(props) {
         </div>
       </div>
 
-    </div>
+    </footer>
   )
 }
