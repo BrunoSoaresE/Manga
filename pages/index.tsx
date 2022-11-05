a1ca75972e7c35401503c411133ef748666051b9
Bruno Soares <bruno.soaest95@gmail.com>
2022-11-05 18:40:38 -0300

    teste123

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,19 +6,15 @@ export default function Home() {
     <div className={styles.container}>
       <Head>
         <title>TecManga - Veja como ter um site sem preocupações</title>
-        <meta property="og:title" content="TecManga - Sites personalizados" />
 
         <meta name='description' content='Seu site está próximo de se tornar realidade a TecManga vai ajuda-lo a tornar seu sonho realidade sem dor de cabeça' />
-        <meta property="og:description" content="Seu site está próximo de se tornar realidade a TecManga vai ajuda-lo a tornar seu sonho realidade sem dor de cabeça." />
 
-        <meta property="og:image" content="https://ahrefs.com/blog/wp-content/uploads/2019/12/fb-how-to-become-an-seo-expert.png" />
         <link rel='icon' href='/assets/logos/LogoBola.png' />
 
         <meta name="author" content="TecManga" />
         <meta name="copyright" content="© 2022 TecManga" />
 
-        <meta property='og:url' content='https://tecmanga.com.br/' />
-        <meta property='og:type' content='website' />
+        <meta property='og:url' content='tecmanga.com.br/' />
         <meta http-equiv="content-language" content="pt-br, en-US" />*/
       </Head>
 
